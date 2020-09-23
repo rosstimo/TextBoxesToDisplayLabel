5fbcec96815313a062dbcebc32eb17a889e12fc0
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-09-23 08:42:02 -0600

    add validate text boxes

--- TextBoxesToDisplayLabelForm.vb
+++ TextBoxesToDisplayLabelForm.vb
@@ -5,24 +5,65 @@
     End Sub
 
     Private Sub DisplayButton_Click(sender As Object, e As EventArgs) Handles DisplayButton.Click
+        ValidateTextBoxes()
+
+        If AccumulateMessage("", False) <> "" Then 'TODO ugly, fix?
+            AlertUser(AccumulateMessage("", False))
+        End If
+
+        'CreateEmail() 'Don't generate if alert user
         Display()
+
     End Sub
 
-    Sub Display()
-        'TODO Validate Textbox data
+    Sub ValidateTextBoxes()
+
+        If LastNameTextBox.Text = "" Then
+            AccumulateMessage("Last Name is a required field", False)
+            LastNameTextBox.Focus()
+        End If
+
+        If FirstNameTextBox.Text = "" Then
+            AccumulateMessage("First Name is a required field", False)
+            FirstNameTextBox.Focus()
+        End If
+
+
         If EmailTextBox.Text = "" Then
             CreateEmail()
         End If
-        DisplayLabel.Text = FirstNameTextBox.Text _
-                            & vbNewLine & LastNameTextBox.Text _
-                            & vbNewLine & EmailTextBox.Text
+
     End Sub
+
+    Sub AlertUser(ByVal message As String)
+        MsgBox(message)
+        AccumulateMessage("", True) 'TODO move?
+    End Sub
+
+    Function AccumulateMessage(ByVal newMessage As String, ByVal Clear As Boolean) As String
+        Static message As String
+
+        If Clear Then
+            message = ""
+        Else
+            message &= newMessage & vbNewLine
+        End If
+
+        Return message
+    End Function
 
     Sub CreateEmail()
         EmailTextBox.Text = FirstNameTextBox.Text _
                             & "." & LastNameTextBox.Text _
                             & "@acme.com"
     End Sub
+
+    Sub Display()
+        DisplayLabel.Text = FirstNameTextBox.Text _
+                            & vbNewLine & LastNameTextBox.Text _
+                            & vbNewLine & EmailTextBox.Text
+    End Sub
+
 
 
 End Class
